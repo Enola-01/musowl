4c2b4dbf1206b02b2537666edce14f746717a92c
Alone <85036805+Enola-01@users.noreply.github.com>
2023-04-07 09:55:07 +0300

    Add files via upload

---
+++ settings/ayarlar.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN || "",  // bot token gir :)
+    PREFIX: process.env.PREFIX || "!", // bot prefix
+    SAHİP: process.env.SAHİP || "822770318740553729", //sahip id :()
+}
